16b15a79c5fbb39789f9a5b740970b32130d5dc3
saman <abdallaelsaman67@gmail.com>
2019-02-14 14:14:55 +0200

    adding drawing mechanism

--- Set/ViewController.swift
+++ Set/ViewController.swift
@@ -36,6 +36,7 @@ class ViewController: UIViewController {
         for index in 0..<playingCards.count {
             playingCards[index].layer.cornerRadius = 5
         }
+        draw()
     }
     
     @IBAction func touchCard(_ sender: SetCard) {
@@ -55,6 +56,83 @@ class ViewController: UIViewController {
         
     }
     
+    func draw () {
+        let text: String
+        let font = UIFont.systemFont(ofSize: 30)
+        var attributes : [NSAttributedString.Key : Any] = [
+           .font: font
+        ]
+        
+        let card = Card(
+            number: Card.varient(rawValue: 2)!,
+            color: Card.varient(rawValue: 2)!,
+            symbol: Card.varient(rawValue: 3)!,
+            shading: Card.varient(rawValue: 3)!
+        )
+        
+        switch  card.symbol {
+        case .one:
+            switch  card.number {
+            case .one:
+                text = "▲"
+            case .two:
+                text = "▲▲"
+            case .three:
+                text = "▲▲▲"
+            }
+        case .two:
+            switch  card.number {
+            case .one:
+                text = "■"
+            case .two:
+                text = "■■"
+            case .three:
+                text = "■■■"
+            }
+        case .three:
+            switch  card.number {
+            case .one:
+                text = "●"
+            case .two:
+                text = "●●"
+            case .three:
+                text = "●●●"
+            }
+        }
+        
+        switch  card.color {
+        case .one:
+            switch  card.shading {
+            case .one:
+                attributes[.foregroundColor] = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1).withAlphaComponent(0.25)
+            case .two:
+                attributes[.foregroundColor] = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1).withAlphaComponent(0.65)
+            case .three:
+                attributes[.foregroundColor] = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1).withAlphaComponent(1.0)
+            }
+        case .two:
+            switch  card.shading {
+            case .one:
+                attributes[.foregroundColor] = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1).withAlphaComponent(0.25)
+            case .two:
+                attributes[.foregroundColor] = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1).withAlphaComponent(0.65)
+            case .three:
+                attributes[.foregroundColor] = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1).withAlphaComponent(1.0)
+            }
+        case .three:
+            switch  card.shading {
+            case .one:
+                attributes[.foregroundColor] = #colorLiteral(red: 0, green: 1, blue: 0, alpha: 1).withAlphaComponent(0.25)
+            case .two:
+                attributes[.foregroundColor] = #colorLiteral(red: 0, green: 1, blue: 0, alpha: 1).withAlphaComponent(0.65)
+            case .three:
+                attributes[.foregroundColor] = #colorLiteral(red: 0, green: 1, blue: 0, alpha: 1).withAlphaComponent(1.0)
+            }
+        }
+        
+        let attributedString = NSAttributedString(string: text, attributes: attributes)
+        playingCards[0].setAttributedTitle(attributedString, for: .normal)
+    }
     
 }
 
